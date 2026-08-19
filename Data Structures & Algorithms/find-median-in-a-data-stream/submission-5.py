09047874af3c4d6dea51b708c7e25d079df21a4d
nitantkhairwar <khairwarnitant@gmail.com>
2026-08-19 15:29:44 +0530

    Add: find-median-in-a-data-stream - submission-5

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-5.py
@@ -0,0 +1,18 @@
+import bisect
+class MedianFinder:
+
+    def __init__(self):
+        self.arr = []
+
+    def addNum(self, num: int) -> None:
+        bisect.insort(self.arr, num)
+
+    def findMedian(self) -> float:
+        n = len(self.arr)
+        if n%2 == 0:
+            mid = n//2
+            median = (self.arr[mid] + self.arr[mid-1])/2
+        else:
+            mid = n//2
+            median = self.arr[mid]
+        return median
